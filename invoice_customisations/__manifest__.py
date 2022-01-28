9e51aa460fc96126100d91a2e445df88ed22ec74
pmcplcj <97661777+pmcplcj@users.noreply.github.com>
2022-01-28 18:44:54 +0530

    Added invoice customisation
    Added invoice customisation

---
+++ invoice_customisations/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Invoice Customisations',
+    'version': '1.1',
+    'summary': 'Inherit fields module',
+    'sequence': 5,
+    'category': 'Tools',
+    'description': 'Inherit fields module',
+    'website': '',
+    'depends': ['base', 'account'],
+    'data': [
+       "views/invoice.xml"
+    ],
+    'demo': [],
+    'q web': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
